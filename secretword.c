d41ee9b40e2ebdde91bb6a0df1463c01508f5606
Covenant <cfaluyi@gmail.com>
2020-10-09 17:21:03 -0400

    Added function to copy words from dictionary to wordsList array

--- secretword.c
+++ secretword.c
@@ -14,12 +14,37 @@ int main(int argc, char *argv[]) {
 
     if (file == NULL){
         printf("Could not open file %s\n", fileName);
-        return 0;
+        return 1;
     }
 
     for (newLineChar = getc(file); newLineChar != EOF; newLineChar = getc(file)) 
         if (newLineChar == '\n') // Increment count if this character is newline 
             wordCount =  wordCount + 1; 
+
+    
+    char wordsList[wordCount][1024];
+    char * line = NULL;
+    size_t len = 0;
+    int currentLine = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, file)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+        strcpy(wordsList[currentLine], line);
+        currentLine += 1;
+    }
+
+    fclose(file);
+    if(line)
+        free(line);
+    
+    
+    
+    return 0;
+
+
+
     
 
 
