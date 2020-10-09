d686004b1befbeabd2b40276185aa9882c89d6fe
Covenant <cfaluyi@gmail.com>
2020-10-09 17:07:43 -0400

    wordCount function

--- secretword.c
+++ secretword.c
@@ -1,3 +1,28 @@
-int main(int argc, char const *argv[]) {
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main(int argc, char *argv[]) {
+    char* fileName = argv[1];
+    printf("filename is %s \n", fileName);
+
+    FILE *file;
+    char newLineChar;
+    int wordCount = 0;
+
+    file = fopen(fileName, "r");
+
+    if (file == NULL){
+        printf("Could not open file %s\n", fileName);
+        return 0;
+    }
+
+    for (newLineChar = getc(file); newLineChar != EOF; newLineChar = getc(file)) 
+        if (newLineChar == '\n') // Increment count if this character is newline 
+            wordCount =  wordCount + 1; 
     
+    printf("The file %s has %d lines\n ", fileName, wordCount); 
+
+
+
 }
