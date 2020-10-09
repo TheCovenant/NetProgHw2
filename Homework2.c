77e6971145207add0bd0afc8cd29f0806cc4c356
Chang Ju Kim <kimc13@rpi.edu>
2020-10-09 18:35:06 -0400

    Comment changes

--- Homework2.c
+++ Homework2.c
@@ -50,9 +50,11 @@ char* lowercase(char* word) {
     1. words/guesses are lowercase (so that you don't call lowercase each function call)
     2. <sortedWord> is <word> sorted. (This is so that you don't sort the word each function call
  */
-// Things I want to add: Ensuring there aren't any non-letter characters
-// lowercase...? I mean i was planning for the main function to lowercase stuff
-// so that you don't have to each time
+// Things I want to add:
+//   Ensuring there aren't any non-letter characters
+//   Lowercase...? I mean i was planning for the main function to lowercase stuff
+//     so that you don't have to each time response is called
+//   You can also return the error message instead of returning void (and printing the error message)
 void response(char* user, char* word, char* sortedWord, char* guess) {
   if (strlen(word) != strlen(guess)) {
     printf("Invalid guess length. The secret word is %d letter(s).\n", (int)strlen(word));
@@ -95,14 +97,7 @@ void response(char* user, char* word, char* sortedWord, char* guess) {
 
 
 int main(int argc, int** argv) {
-  char* word = "HELLo!";
-  printf("%s\n", word);
-  word = lowercase(word);
-  printf("%s\n", word);
-  char* sortedWord = sort(word);
-  printf("%s\n", sortedWord);
-  
-  
+ 
   response("bob", "guess", sort("guess"), "snipe");
   response("bob", "guess", sort("guess"), "cross");
   response("bob", "gruel", sort("gruel"), "spill");
