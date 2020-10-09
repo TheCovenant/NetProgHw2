bd6397e9ea8f5ae4534dd30e797fac54e8f528eb
Covenant <cfaluyi@gmail.com>
2020-10-09 18:10:27 -0400

    Read line and store in wordList array

--- secretword.c
+++ secretword.c
@@ -16,30 +16,41 @@ int main(int argc, char *argv[]) {
         printf("Could not open file %s\n", fileName);
         return 1;
     }
-
     for (newLineChar = getc(file); newLineChar != EOF; newLineChar = getc(file)) 
         if (newLineChar == '\n') // Increment count if this character is newline 
             wordCount =  wordCount + 1; 
 
-    
-    char wordsList[wordCount][1024];
+    fclose(file);
+    free(file);
+
+    printf("we have %d words\n", wordCount);
+    char **wordsList = (char**)malloc(wordCount * sizeof(char*));
+    for (int x = 0; x< wordCount; x++)
+        wordsList[x]= (char *)malloc( 1024 * sizeof(char));
     char * line = NULL;
     size_t len = 0;
     int currentLine = 0;
     ssize_t read;
 
+    file = fopen(fileName, "r");
     while ((read = getline(&line, &len, file)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        printf("%s", line);
+        //printf("Retrieved line of length %zu:\n", read);
+        //printf("%s", line);
         strcpy(wordsList[currentLine], line);
         currentLine += 1;
+        if (currentLine == wordCount)
+            break;
+        
     }
 
     fclose(file);
     if(line)
         free(line);
+    printf("First word is %s\n", wordsList[0]);
+    printf("Last word is %s\n", wordsList[wordCount-1]);
+
     
-    
+
     
     return 0;
 
