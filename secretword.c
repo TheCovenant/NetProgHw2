de08800f736942b415982ccad62cad5546a20751
Covenant <cfaluyi@gmail.com>
2020-10-11 15:14:24 -0400

    moved main code into functions

--- secretword.c
+++ secretword.c
@@ -2,25 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-int main(int argc, char *argv[]) {
-    int seedNumber;
-    int longestWordLength;
-    sscanf(argv[1], "%d", &seedNumber);
-    sscanf(argv[3], "%d", &longestWordLength);
-    srand(seedNumber);
 
-    char* fileName = argv[2];
-    printf("filename is %s \n", fileName);
-
+void getWordCount(char* fileName, int* wordCount){
     FILE *file;
     char newLineChar;
-    int wordCount = 0;
 
     file = fopen(fileName, "r");
 
     if (file == NULL){
         printf("Could not open file %s\n", fileName);
-        return 1;
+        return;
     }
     for (newLineChar = getc(file); newLineChar != EOF; newLineChar = getc(file)) 
         if (newLineChar == '\n') // Increment count if this character is newline 
@@ -28,15 +19,18 @@ int main(int argc, char *argv[]) {
 
     fclose(file);
     free(file);
+}
 
-    printf("we have %d words\n", wordCount);
-    char **wordsList = (char**)malloc(wordCount * sizeof(char*));
+char** getDictionaryWords(int longestWordLength, int wordCount, char* fileName) {
+    char** wordsList = (char**)malloc(wordCount * sizeof(char*));
     for (int x = 0; x< wordCount; x++)
         wordsList[x]= (char *)malloc( longestWordLength * sizeof(char));
+
     char * line = NULL;
     size_t len = 0;
     int currentLine = 0;
     ssize_t read;
+    FILE *file;
 
     file = fopen(fileName, "r");
     while ((read = getline(&line, &len, file)) != -1) {
@@ -50,16 +44,36 @@ int main(int argc, char *argv[]) {
     fclose(file);
     if(line)
         free(line);
+
+    
+
+    return wordsList;
+}
+ 
+
+int main(int argc, char *argv[]) {
+    int seedNumber;
+    int longestWordLength;
+    sscanf(argv[1], "%d", &seedNumber);
+    sscanf(argv[3], "%d", &longestWordLength);
+    srand(seedNumber);
+    char* fileName = argv[2];
+    printf("filename is %s \n", fileName);
+
+    int wordCount = 0;
+    getWordCount(fileName, &wordCount);
+
+    printf("we have %d words\n", wordCount);
+    char **wordsList = getDictionaryWords(longestWordLength, wordCount, fileName);
+
+    
+   
     printf("First word is %s\n", wordsList[0]);
     printf("Last word is %s\n", wordsList[wordCount-1]);
 
     unsigned int secretWordLocation = rand() % wordCount;
 
     printf("secret word is %s\n", wordsList[secretWordLocation]);
-
-
-
-    
 
     
     return 0;
